99da47d5115c442f95040e0746962678416f6e72
SanczoPL <grzegorz600425@gmail.com>
2019-11-10 18:35:17 +0100

    add new file to cmake

--- App/main.cpp
+++ App/main.cpp
@@ -1,15 +1,15 @@
+#include "calculatorlogic.h"
 #include "widget.h"
-#include <calculatorlogic.h>
 
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
+  QApplication a(argc, argv);
 
-    CalculatorLogic cl;
+  CalculatorLogic cl;
 
-    Widget w;
-    w.show();
-    return a.exec();
+  Widget w;
+  w.show();
+  return a.exec();
 }
